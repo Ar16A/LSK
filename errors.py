0daa01e809b599dfb786c68fcabf74e3fdd4e0c9
GlavPost <mr.mastik31@gmail.com>
2025-04-23 15:32:48 +0300

    Update errors

--- errors.py
+++ errors.py
@@ -1,10 +1,19 @@
-class OccipiedEmail(ValueError):
-    pass
+class OccupiedEmail(ValueError):
+    def __init__(self, email):
+        self.email = email
+
+    def __str__(self):
+        return f"Пользователь с почтой {self.email} уже существует"
 
 
-class OccipiedUsername(ValueError):
-    pass
+class OccupiedUsername(ValueError):
+    def __init__(self, username):
+        self.username = username
+
+    def __str__(self):
+        return f"Пользователь с именем {self.username} уже существует"
 
 
-class OccipiedUsernameAndEmail(ValueError):
-    pass
+class OccupiedUsernameAndEmail(ValueError):
+    def __str__(self):
+        return f"Такой пользователь уже зарегистрирован"
