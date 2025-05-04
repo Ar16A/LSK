d4e26671c55c2e8f9bb391caf1475442ab57902a
GlavPost <mr.mastik31@gmail.com>
2025-05-05 02:56:45 +0300

    Update User 0.6 (add folder methods)

--- errors.py
+++ errors.py
@@ -1,5 +1,6 @@
 class OccupiedEmail(ValueError):
     """Ошибка: в БД уже есть пользователь с такой почтой"""
+
     def __init__(self, email):
         self.email = email
 
@@ -9,6 +10,7 @@ class OccupiedEmail(ValueError):
 
 class OccupiedUsername(ValueError):
     """Ошибка: в БД уже есть пользователь с такой именем"""
+
     def __init__(self, username):
         self.username = username
 
@@ -18,12 +20,14 @@ class OccupiedUsername(ValueError):
 
 class OccupiedUsernameAndEmail(ValueError):
     """Ошибка: в БД уже есть пользователь с такой именем и почтой"""
+
     def __str__(self):
         return "Такой пользователь уже зарегистрирован"
 
 
 class OccupiedNameNote(ValueError):
     """Ошибка: в БД уже есть заметка с таким именем"""
+
     def __init__(self, name):
         self.name = name
 
@@ -31,8 +35,40 @@ class OccupiedNameNote(ValueError):
         return f"Заметка \"{self.name}\" уже существует. Выберите другое название для заметки"
 
 
+class OccupiedNameFolder(ValueError):
+    """Ошибка: в БД уже есть папка с таким именем"""
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"Папка \"{self.name}\" уже существует. Выберите другое название для папки"
+
+
+class OccupiedName(ValueError):
+    """Ошибка: в БД уже существует объект с таким названием"""
+
+    def __init__(self, type: str, name: str = None):
+        self.name = name
+        self.type = type
+
+    def __str__(self):
+        match self.type:
+            case "username":
+                return f"Пользователь с именем {self.name} уже существует"
+            case "email":
+                return f"Пользователь с почтой {self.name} уже существует"
+            case "all":
+                return "Такой пользователь уже зарегистрирован"
+            case "note":
+                return f"Заметка \"{self.name}\" уже существует. Выберите другое название для заметки"
+            case "folder":
+                return f"Папка \"{self.name}\" уже существует. Выберите другое название для папки"
+
+
 class UserNotExists(ValueError):
     """Ошибка: пользователя с таким логином нет в БД"""
+
     def __init__(self, login):
         self.login = login
 
@@ -42,11 +78,13 @@ class UserNotExists(ValueError):
 
 class IncorrectPassword(ValueError):
     """Ошибка: введённый пароль не совпадает с паролем из БД"""
+
     def __str__(self):
         return "Неверный пароль"
 
 
 class NotChange(ValueError):
     """Ошибка: в заметку не было внесено изменений"""
+
     def __str__(self):
         return "Заметка не изменена"
