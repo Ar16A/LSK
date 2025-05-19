00c7b40f3e9b9283ab293fce5a29ce8bb052aa3f
Arina <alejba75@gmail.com>
2025-05-19 17:20:23 +0300

    Update summary() 0.2

--- summarize.py
+++ summarize.py
@@ -1,18 +1,19 @@
 import time
+
+from httpx import ConnectError
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_gigachat.chat_models import GigaChat
 
+class TooMany(RuntimeError):
+    def __str__(self):
+        return "Слишком много запросов. Сервер устал. Попробуйте позже."
 
-# Функции для ошибок
-def TooMany():
-    return "Слишком много запросов. Сервер устал. Попробуйте позже."
+class NotEthical(ValueError):
+    def __str__(self):
+        return "Gigachat отказался отвечать по моральным соображениям."
 
 
-def NotEthical():
-    return "Gigachat отказался отвечать по моральным соображениям."
-
-
-def summarize(text: str) -> str:
+def summary(text: str) -> str:
     """
     Генерирует краткое содержание текста
     """
@@ -20,35 +21,25 @@ def summarize(text: str) -> str:
         credentials="MDNiZWNiMmMtZWE5MS00MDY3LTlkYTAtNmQ5MjE0NmI3M2ViOjg5YTMxZjk1LTY1ZjYtNGE2YS1iYzZkLWRkMTlhYzFhMWQ1Mw==",
         verify_ssl_certs=False,
     )
-
     messages = [
         SystemMessage(content="""Ты профессиональный суммаризатор текстов. 
                        Сгенерируй краткое содержание на русском языке.""")
     ]
-
     messages.append(HumanMessage(content=text))
-
     try:
         response = chat.invoke(messages)
-        summary = response.content
-
+        answer = response.content
         # Проверяем все возможные случаи отказа
-        if ("этич" in summary.lower() or
-                "отказываюсь" in summary.lower() or
+        if ("этич" in answer.lower() or
+                "отказываюсь" in answer.lower() or
                 "blacklist" in str(response) or
-                "Не люблю менять тему" in summary):
-            return NotEthical()
-
-        return summary
-
+                "Не люблю менять тему" in answer):
+            raise NotEthical
+        return answer
+    except ConnectError:
+        raise ConnectError
+    except NotEthical:
+        raise NotEthical
     except Exception as e:
         if "TooMany" in str(e) or "429" in str(e):
-            return TooMany()
-
-        print(f"Ошибка при суммаризации: {e}")
-        return ""
-
-
-
-
-
+            raise TooMany
